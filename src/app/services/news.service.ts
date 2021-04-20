a25555907f72cff04a950e9fe9065b25890084ee
Taylin Squire <taylinsquire@gmail.com>
2021-04-20 09:53:15 -0600

    Add news service

---
+++ src/app/services/news.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+
+const apiKey = '563a5794042c457580326ddc1d8d139b';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsService {
+  constructor(private http: HttpClient) {}
+
+  getTopStoriesObservable(
+    page: number = 1,
+    category: string = ''
+  ): Observable<any> {
+    console.log(category);
+    if (!category) {
+      console.log('non-category search');
+      return this.http.get(
+        `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&page=${page}&apiKey=${apiKey}`
+      );
+    }
+    console.log('category search');
+    return this.http.get(
+      `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&category=${category}&page=${page}&apiKey=${apiKey}`
+    );
+  }
+
+  getStoriesBySearchObservable(): Observable<any> {
+    return this.http.get(`https://newsapi.org/v2/everything?apiKey=${apiKey}`);
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsService } from '../services/news.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  stories$;
+  page: number = 1;
+  constructor(private newsService: NewsService) {
+    this.stories$ = this.newsService.getTopStoriesObservable(this.page);
+  }
 
-  constructor() {}
-
+  onCategoryChange($event) {
+    this.stories$ = this.newsService.getTopStoriesObservable(this.page, $event.detail.value);
+  }
 }
