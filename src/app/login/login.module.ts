88fbb193363351d85804be3e9f447d86c86e09c9
Brayden Barger <brayden.barger8273@stu.mtec.edu>
2021-04-21 13:15:30 -0600

    authentication

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    NgxAuthFirebaseUIModule
   ],
   declarations: [LoginPage]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBbPWdTMomjxZD9C-Bzi7tnZ7W6cC18BB8",
+    authDomain: "news-app-eff9f.firebaseapp.com",
+    projectId: "news-app-eff9f",
+    storageBucket: "news-app-eff9f.appspot.com",
+    messagingSenderId: "734373268640",
+    appId: "1:734373268640:web:649696f1e0ab3be22f4dc5"
+  }
 };
 
 /*
