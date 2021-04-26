fb4dc5a9049dd70c6d73adbe7153670ca3d3f6dc
Taylin Squire <taylinsquire@gmail.com>
2021-04-26 08:33:42 -0600

    Move api key to http headers

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -16,26 +16,75 @@ export class NewsService {
     category: string = ''
   ): Observable<any> {
     if (!category) {
-      return this.http.get(
-        `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&page=${page}&apiKey=${apiKey}`
-      ).pipe(shareReplay());
+      return this.http
+        .get(
+          `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&page=${page}`,
+          {
+            headers: {
+              'X-Api-Key': apiKey,
+            },
+          }
+        )
+        .pipe(shareReplay());
     }
-    return this.http.get(
-      `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&category=${category}&page=${page}&apiKey=${apiKey}`
-    ).pipe(shareReplay());
+    return this.http
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&category=${category}&page=${page}`,
+        {
+          headers: {
+            'X-Api-Key': apiKey,
+          },
+        }
+      )
+      .pipe(shareReplay());
   }
 
-  getStoriesBySearchObservable(search: string = 'news', category: string = ''): Observable<any> {
+  getStoriesBySearchObservable(
+    search: string = 'news',
+    category: string = ''
+  ): Observable<any> {
     if (category) {
       if (search === '') {
-        return this.http.get(`https://newsapi.org/v2/top-headlines?q=news&category=${category}&apiKey=${apiKey}`).pipe(shareReplay());
-        }
-        return this.http.get(`https://newsapi.org/v2/top-headlines?q=${search}&category=${category}&apiKey=${apiKey}`).pipe(shareReplay());
+        return this.http
+          .get(
+            `https://newsapi.org/v2/top-headlines?q=news&category=${category}`,
+            {
+              headers: {
+                'X-Api-Key': apiKey,
+              },
+            }
+          )
+          .pipe(shareReplay());
+      }
+      return this.http
+        .get(
+          `https://newsapi.org/v2/top-headlines?q=${search}&category=${category}`,
+          {
+            headers: {
+              'X-Api-Key': apiKey,
+            },
+          }
+        )
+        .pipe(shareReplay());
     }
 
     if (search === '') {
-    return this.http.get(`https://newsapi.org/v2/everything?q=news&apiKey=${apiKey}`).pipe(shareReplay());
+      return this.http
+        .get(`https://newsapi.org/v2/everything?q=news`,
+        {
+            headers: {
+              'X-Api-Key': apiKey,
+            },
+          })
+        .pipe(shareReplay());
     }
-    return this.http.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=${apiKey}`).pipe(shareReplay());
+    return this.http
+      .get(`https://newsapi.org/v2/everything?q=${search}`,
+      {
+            headers: {
+              'X-Api-Key': apiKey,
+            },
+          })
+      .pipe(shareReplay());
   }
 }
